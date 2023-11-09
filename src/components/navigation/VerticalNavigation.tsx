fd2a08a0c39cbd1ad300eaf5f66cdf1ea555f72e
tony watters <tony.watters@learningpool.com>
2023-11-09 14:38:52 +0000

    comment out the navigation app

--- src/components/navigation/VerticalNavigation.tsx
+++ src/components/navigation/VerticalNavigation.tsx
@@ -1,12 +1,12 @@
 import { VerticalNavigation } from '@learningpool/ui'
 
+// old import for testing the learning pool ui
+// function App() {
+//   return (
+//     <div className="App">
+//        <VerticalNavigation />
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-       <VerticalNavigation />
-    </div>
-  );
-}
-
-export default App;
+// export default App;
