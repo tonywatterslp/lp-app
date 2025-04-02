d63f67d38175abe7149e1779c4832f067f681dea
Pete Thorne <pete.thorne@learningpool.com>
2025-04-02 14:20:12 +0100

    Adding new icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 import { VerticalNavigation, ThemeProvider } from "@learningpool/ui";
-import { Button, Stack, Typography, createTheme } from "@mui/material";
+import { Button, Stack, SvgIcon, Typography, createTheme } from "@mui/material";
 import CourseCreator from "./components/CourseCreator/CourseCreator";
 import CourseTable, { Course } from "./components/CourseTable/CourseTable";
 
@@ -10,6 +10,14 @@ import LearningExperiencesIcon from "./assets/icons/LearningExperiencesIcon.svg"
 import ResourcesIcon from "./assets/icons/ResourcesIcon.svg";
 import PlaylistsIcon from "./assets/icons/PlaylistsIcon.svg";
 import SkillsIcon from "./assets/icons/SkillsIcon.svg";
+import UsersIcon from "./assets/icons/UsersIcon.svg";
+import GroupsIcon from "./assets/icons/GroupsIcon.svg";
+import EventsIcon from "./assets/icons/EventsIcon.svg";
+import SettingsIcon from "./assets/icons/SettingsIcon.svg";
+import SupportIcon from "./assets/icons/SupportIcon.svg";
+import GoToLearnerDashboardIcon from "./assets/icons/GoToLearnerDashboardIcon.svg";
+import OrganisationListIcon from "./assets/icons/OrganisationListIcon.svg";
+import LogoutIcon from "./assets/icons/LogoutIcon.svg";
 
 const DEFAULT_HEADER_TYPOGRAPHY = {
   fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
@@ -200,22 +208,108 @@ function App() {
     {
       label: "Learning Experiences",
       onClick: () => (window.location.href = "/learning-experiences"),
-      icon: <LearningExperiencesIcon />,
+      icon: (
+        <SvgIcon sx={{ width: 32, height: 32 }}>
+          <LearningExperiencesIcon />
+        </SvgIcon>
+      ),
     },
     {
       label: "Resources",
       onClick: () => (window.location.href = "/resources"),
-      icon: <ResourcesIcon />,
+      icon: (
+        <SvgIcon sx={{ width: 32, height: 32 }}>
+          <ResourcesIcon />
+        </SvgIcon>
+      ),
+    },
+    {
+      label: "Events",
+      onClick: () => (window.location.href = "/events"),
+      icon: (
+        <SvgIcon sx={{ width: 32, height: 32 }}>
+          <EventsIcon />
+        </SvgIcon>
+      ),
     },
     {
       label: "Playlists",
       onClick: () => (window.location.href = "/playlists"),
-      icon: <PlaylistsIcon />,
+      icon: (
+        <SvgIcon sx={{ width: 32, height: 32 }}>
+          <PlaylistsIcon />
+        </SvgIcon>
+      ),
     },
     {
-      label: "Skills",
-      onClick: () => (window.location.href = "/skills"),
-      icon: <SkillsIcon />,
+      label: "Users",
+      onClick: () => (window.location.href = "/users"),
+      icon: (
+        <SvgIcon sx={{ width: 32, height: 32 }}>
+          <UsersIcon />
+        </SvgIcon>
+      ),
+    },
+    {
+      label: "Groups",
+      onClick: () => (window.location.href = "/groups"),
+      icon: (
+        <SvgIcon sx={{ width: 32, height: 32 }}>
+          <GroupsIcon />
+        </SvgIcon>
+      ),
+    },
+    {
+      label: "Settings",
+      onClick: () => (window.location.href = "/settings"),
+      icon: (
+        <SvgIcon sx={{ width: 32, height: 32 }}>
+          <SettingsIcon />
+        </SvgIcon>
+      ),
+    },
+  ];
+
+  const secondaryNavItems = [
+    {
+      label: "Support",
+      onClick: () => (window.location.href = "/support"),
+      icon: (
+        <SvgIcon sx={{ width: 32, height: 32 }}>
+          <SupportIcon />
+        </SvgIcon>
+      ),
+    },
+    {
+      label: "Learner Dashboard",
+      onClick: () => (window.location.href = "/learner-dashboard"),
+      icon: (
+        <SvgIcon sx={{ width: 32, height: 32 }}>
+          <GoToLearnerDashboardIcon />
+        </SvgIcon>
+      ),
+    },
+    {
+      label: "Organisation List",
+      onClick: () => (window.location.href = "/organisation-list"),
+      icon: (
+        <SvgIcon sx={{ width: 32, height: 32 }}>
+          <OrganisationListIcon />
+        </SvgIcon>
+      ),
+    },
+    {
+      label: "Logout",
+      onClick: () => {
+        // Perform logout action here
+        console.log("Logging out...");
+        window.location.href = "/logout";
+      },
+      icon: (
+        <SvgIcon sx={{ width: 32, height: 32 }}>
+          <LogoutIcon />
+        </SvgIcon>
+      ),
     },
   ];
 
@@ -240,7 +334,10 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App" style={{ padding: "20px", paddingLeft: "100px" }}>
-        <VerticalNavigation items={navItems} />
+        <VerticalNavigation
+          items={navItems}
+          secondaryItems={secondaryNavItems}
+        />
         <Stack direction={"row"} marginBottom={2}>
           <Typography flexGrow={1} variant="h5" component="h1">
             Learning Experiences
